e745a4222c01f6babd82c01894711b0d1881acda
sriVishalS <vishalS221202@gmail.com>
2022-10-02 13:07:09 +0530

    Responsive Portfolio Website

---
+++ script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
